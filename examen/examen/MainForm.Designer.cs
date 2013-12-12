4282f86ce142cb230adb19420bfd8260954c00f3
dracule-mihawk <j_m_rg@hotmail.es>
2013-12-11 18:55:34 -0600

    practica6

---
+++ examen/examen/MainForm.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace examen
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.checkBox3 = new System.Windows.Forms.CheckBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+			this.label10 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label11 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(19, 18);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(82, 26);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Estado";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+									"Jalisco",
+									"Nayarit",
+									"Sonora"});
+			this.comboBox1.Location = new System.Drawing.Point(19, 34);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(76, 21);
+			this.comboBox1.TabIndex = 1;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Location = new System.Drawing.Point(19, 83);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(21, 18);
+			this.checkBox1.TabIndex = 2;
+			this.checkBox1.Text = "aleman";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.Location = new System.Drawing.Point(19, 107);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(19, 18);
+			this.checkBox2.TabIndex = 3;
+			this.checkBox2.Text = "ingles";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// checkBox3
+			// 
+			this.checkBox3.Location = new System.Drawing.Point(19, 131);
+			this.checkBox3.Name = "checkBox3";
+			this.checkBox3.Size = new System.Drawing.Size(20, 18);
+			this.checkBox3.TabIndex = 4;
+			this.checkBox3.Text = "irlandes";
+			this.checkBox3.UseVisualStyleBackColor = true;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(41, 85);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(53, 20);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Aleman";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(41, 109);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(54, 18);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Ingles";
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(41, 133);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(50, 21);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Español";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(20, 63);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(73, 20);
+			this.label5.TabIndex = 8;
+			this.label5.Text = "Idiomas";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Location = new System.Drawing.Point(20, 196);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(17, 21);
+			this.radioButton1.TabIndex = 9;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "hombre";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.Location = new System.Drawing.Point(20, 223);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(20, 21);
+			this.radioButton2.TabIndex = 10;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "mujer";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(41, 201);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(54, 26);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "hombre";
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(41, 227);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(50, 20);
+			this.label7.TabIndex = 12;
+			this.label7.Text = "mujer";
+			// 
+			// label8
+			// 
+			this.label8.Location = new System.Drawing.Point(19, 166);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(60, 27);
+			this.label8.TabIndex = 13;
+			this.label8.Text = "Sexo";
+			// 
+			// label9
+			// 
+			this.label9.Location = new System.Drawing.Point(196, 18);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(77, 30);
+			this.label9.TabIndex = 14;
+			this.label9.Text = "fecha de nacimiento";
+			// 
+			// monthCalendar1
+			// 
+			this.monthCalendar1.Location = new System.Drawing.Point(196, 45);
+			this.monthCalendar1.Name = "monthCalendar1";
+			this.monthCalendar1.TabIndex = 15;
+			// 
+			// label10
+			// 
+			this.label10.Location = new System.Drawing.Point(19, 254);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(55, 24);
+			this.label10.TabIndex = 16;
+			this.label10.Text = "Edad";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(19, 272);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(38, 20);
+			this.numericUpDown1.TabIndex = 17;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(93, 230);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(330, 81);
+			this.textBox1.TabIndex = 18;
+			// 
+			// label11
+			// 
+			this.label11.Location = new System.Drawing.Point(93, 209);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(81, 18);
+			this.label11.TabIndex = 19;
+			this.label11.Text = "Observaciones";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(208, 317);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(65, 21);
+			this.button1.TabIndex = 20;
+			this.button1.Text = "Guardar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.ClientSize = new System.Drawing.Size(480, 368);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.monthCalendar1);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.checkBox3);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Examen";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.MonthCalendar monthCalendar1;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.CheckBox checkBox3;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+		
+		void Button1Click(object sender, System.EventArgs e)
+		{
+			string sexo = "";
+			if(this.radioButton1.Checked)
+				sexo += "Masculino";
+			if(this.radioButton2.Checked)
+				sexo += "femenino";
+			string edad = "";	
+		}
+	}
+}
---
+++ examen/examen/MainForm.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: JMRG
+ * Date: 29/10/2013
+ * Time: 05:31 p.m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace examen
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+			
+			
+	}
+}
